b7bf40af3a1de7b626e065182b4e12490fb5ceb9
Alessani <alessaniantoniazzi@gmail.com>
2023-12-30 16:59:48 -0300

    refactor: signup restructured

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,21 +1,3 @@
-// import { Request, Response } from 'express';
-// import * as signupService from '../services/signupServices';
-
-// interface CreateUserInput extends Request {
-//     body: signupService.CreateUserInput;
-// }
-
-// export const createUser = async (req: CreateUserInput, res: Response) => {
-//     try {
-//         const newUser = await signupService.createUser(req.body);
-
-//         res.status(201).send(newUser);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send({ message: 'Something went wrong' });
-//     }
-// }
-
 import { Request, Response } from 'express';
 import { UserService } from '../services/signupServices';
 import UserModel, { UserCreate } from '../models/models';
